f4bed3f4c4e746062465f8e705bb3988044cf432
Sumer Malhotra <sumermalhotra1998@gmail.com>
2022-03-01 11:42:24 +0530

    add redis caching

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from dotenv import load_dotenv; load_dotenv()
 
 from utils import *
+
+import aioredis
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.decorator import cache
+from fastapi_cache.backends.redis import RedisBackend
 
 alert(f"{os.getenv('NAME')}: Starting server for Upshot")
 app = FastAPI()
 logger = setup_custom_logger('root')
 setup_file_logger(logger)
 
+@cache()
+async def get_cache():
+    return 1
+
+@app.on_event("startup")
+async def startup():
+    redis =  aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+@cache(expire=600)
 @app.get("/market_cap")
 async def root(collection_name: str):
     try:
